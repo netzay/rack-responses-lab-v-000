608a822b576c9991bcf170e7bb4a659e1948f109
Ally Netzel <netzelally@gmail.com>
2018-05-25 18:47:37 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     t = Time.now.asctime
 
 
-    if t < "Fri May 25 12:00:00 2018"
+    if t <= "Fri May 25 12:00:00 2018"
       resp.write "Good Morning"
     else
       resp.write "Good Afternoon"
