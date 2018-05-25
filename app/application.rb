64203dd101876dc2dd2877d05f0f079de5849910
Ally Netzel <netzelally@gmail.com>
2018-05-25 18:51:48 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,10 +6,10 @@ class Application
     t = Time.now.asctime
 
 
-    if Time.now < 12:00:00
+    if Time.now <= 12:00:00
       resp.write "Good Morning"
     end
-    if Time.now > 12:00:00  
+    if Time.now >= 12:00:00  
       resp.write "Good Afternoon"
     end
 
